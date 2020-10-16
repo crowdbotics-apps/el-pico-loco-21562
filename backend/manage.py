23c3311c69d17673958d4aa2812711475b6d9784
Crowdbotics <team@crowdbotics.com>
2020-10-16 07:37:14 +0000

    Update models

--- backend/manage.py
+++ backend/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'el_pico_loco_21562.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "el_pico_loco_21562.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,5 +17,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
